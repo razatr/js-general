321d7f7fb0d0bef4dfd4764effb99d95234098ad
Legolas <razatr@yandex.ru>
2019-11-24 14:03:23 +0300

    Ball was solved

---
+++ size-and-scroll/index.js
@@ -0,0 +1,4 @@
+"use strict"
+
+ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
